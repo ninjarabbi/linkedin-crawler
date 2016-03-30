39b6846a643d7ccc2f78c72b4cd7d0d78b880383
Sergey Bukhman <ninjarabbi@gmail.com>
2016-03-31 01:33:16 +0300

    added skills

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -10,11 +10,17 @@ class ProfilesController < ApplicationController
     if @url
       page = Nokogiri::HTML(open(@url, &:read))
 
+      skills = []
+      page.css("#skills li").each do |s|
+        skills.push(s.text)
+      end
+
       render json: {
           title: page.css(".profile-overview-content .title").text,
           name: page.css("#name").text,
           position: page.css(".org a").text,
-          summary: page.css("#summary .description").text
+          summary: page.css("#summary .description").text,
+          skills: skills
       }
 
     else
